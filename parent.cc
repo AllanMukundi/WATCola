9eae8fd8eb7fe8fe33b8d18b493896571510b5b0
Bart Chrzaszcz <bchrzasz@uwaterloo.ca>
2019-11-24 01:30:18 -0500

    Implement Parent.

---
+++ parent.cc
@@ -0,0 +1,28 @@
+#include "parent.h"
+
+extern MPRNG mprng;
+
+Parent::Parent(Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay):
+    bank(bank), printer(prt), numStudents(numStudents), parentalDelay(parentalDelay) {}
+
+void Parent::main() {
+    while (true) {
+        _Accept(Parent::~Parent) {
+            return;
+        } _Else {
+            // Before each gift is transferred, the parent yields for 
+            // `parentalDelay` times (not random).
+            yield(parentalDelay);
+
+            // Get the random student.
+            unsigned studentID = mprng(numStudents - 1);
+
+            // Get the amount of money.
+            // TODO: does mprng include the upper bound?
+            unsigned moneyAmount = mprng(1, 3);
+
+            // Deposit!
+            bank.deposit(studentID, moneyAmount);
+        }
+    }
+}
