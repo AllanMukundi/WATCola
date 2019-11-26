8b3bbe0389129ae8fba2dfae86330fff61e2f6ea
allanmukundi <allanmukundi@hotmail.com>
2019-11-26 17:56:58 -0500

    Finish NameServer

--- name_server.h
+++ name_server.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include "printer.h"
+#include "vending_machine.h"
+
 _Task NameServer {
+    Printer &printer;
+    unsigned numVendingMachines, numStudents;
+    VendingMachine **machines;
+    unsigned *assignment;
 	void main();
   public:
 	NameServer( Printer & prt, unsigned int numVendingMachines, unsigned int numStudents );
+    ~NameServer();
 	void VMregister( VendingMachine * vendingmachine );
 	VendingMachine * getMachine( unsigned int id );
 	VendingMachine ** getMachineList();
