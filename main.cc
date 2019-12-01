3111f62fa70428cf1a2cae42b6f3cb8dc524f0ec
allanmukundi <allanmukundi@hotmail.com>
2019-11-30 19:37:08 -0500

    Write main

--- main.cc
+++ main.cc
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <sstream>
-#include "MPRNG.h"
+
+#include "bank.h"
+#include "bottling_plant.h"
 #include "config.h"
+#include "groupoff.h"
+#include "name_server.h"
+#include "parent.h"
+#include "printer.h"
+#include "student.h"
+#include "wat_card_office.h"
+#include "vending_machine.h"
+#include "MPRNG.h"
 
 using namespace std;
 
@@ -63,4 +73,36 @@ int main(int argc, char *argv[]) {
     // Read config file.
     ConfigParms config;
     processConfigFile(configFile.c_str(), config);
+
+    // Let's go.
+    
+    Printer printer(config.numStudents, config.numVendingMachines, config.numCouriers);
+    Bank bank(config.numStudents); 
+    Parent parent(printer, bank, config.numStudents, config.parentalDelay);
+    WATCardOffice office(printer, bank, config.numCouriers);
+    Groupoff groupoff(printer, config.numStudents, config.sodaCost, config.groupoffDelay);
+    NameServer nameserver(printer, config.numVendingMachines, config.numStudents);
+
+    VendingMachine *machines[config.numVendingMachines];
+    for(unsigned i = 0; i < config.numVendingMachines; ++i) {
+        machines[i] = new VendingMachine(printer, nameserver, i, config.sodaCost);
+    }
+
+    BottlingPlant *plant = new BottlingPlant(printer, nameserver, config.numVendingMachines, config.maxShippedPerFlavour,
+            config.maxStockPerFlavour, config.timeBetweenShipments);
+
+    Student *students[config.numStudents];
+    for(unsigned i = 0; i < config.numStudents; ++i) {
+        students[i] = new Student(printer, nameserver, office, groupoff, i, config.maxPurchases);
+    }
+
+    for(unsigned i = 0; i < config.numStudents; ++i) {
+        delete students[i];
+    }
+
+    delete plant;
+
+    for(unsigned i = 0; i < config.numVendingMachines; ++i) {
+        delete machines[i];
+    }
 }
